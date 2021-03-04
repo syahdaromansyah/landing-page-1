a39a705fa763815c40777e6b2e5f5112cdf16cd0
syuhadarahdiansyah <syuhadarhd@gmail.com>
2021-03-04 16:46:55 +0700

    Updated landing page style and improved script.js code using DOM Delegation.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,16 @@
 //? ## Script for mobile nav ##
-//* Declaring menu bars icon, and mobile nav menu
-const menuBars = document.querySelector('.nav-right > i');
-const menuMobile = document.querySelector('.nav-right > ul.flex');
+const menuMobile = document.querySelector(".nav-right > ul.flex");
 
-//* When menu bars clicked
-menuBars.addEventListener('click', function () {
-  menuMobile.classList.add('active-m-nav');
-});
+document.addEventListener("click", function (e) {
+  if (!e.target.closest(".nav-right")) {
+    menuMobile.classList.remove("active-m-nav");
+  }
 
-menuMobile.addEventListener('click', function (e) {
-  //* When close mobile menu clicked
-  if (e.target.classList.contains('fa-times')) {
-    menuMobile.classList.remove('active-m-nav');
+  if (e.target.classList.contains("fa-bars"))
+    menuMobile.classList.add("active-m-nav");
+
+  if (e.target.classList.contains("fa-times")) {
+    menuMobile.classList.remove("active-m-nav");
     e.preventDefault();
   }
 });
